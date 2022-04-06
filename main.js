8f1456a2719d65936f7a6c6c459b2262bd1b9320
Atira2009 <77487768+Atira2009@users.noreply.github.com>
2022-04-06 17:11:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup(){
+    x = createCapture(VIDEO);
+    x.size(550, 500);
+    c = createCanvas(550, 500);
+    c.position(570, 100);
+    poseNet = ml5.poseNet(x,modelloaded);
+    poseNet.on('pose', gotposes)
+}
+
+function draw(){
+    background("#ADD8E6");
+    textSize(ans);
+    fill(0, 102, 153);
+    text("PoGLiFe",40,200);
+}
+function modelloaded(){
+    console.log("Initialized PoseNet");
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+        rightWrist = results[0].pose.rightWrist.x;
+        leftWrist = results[0].pose.leftWrist.x;
+        ans = floor(leftWrist - rightWrist);
+    }
+}
